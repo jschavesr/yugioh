4febcdedede8ad911e93a565e7dace7c576a7ac3
Sebastian Chaves <jschavesr@unal.edu.co>
2024-02-18 20:43:18 -0500

    Add suppor to extra deck and side deck

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const textContainer = document.getElementById('textContainer');
 const setsSelect = document.getElementById('setsSelect');
 let allCards = {};
 let avalableCards = {};
+const extraTypes = ["Fusion Monster", "Synchro Monster", "XYZ Monster"]
 const budget = 100;
 let deck = {
     main: []
@@ -91,24 +92,24 @@ function parseYdkeUrl(ydke) {
 }
 
 function passcodesToBase64(list_id) {
-  const passcodes = new Uint32Array(list_id);
-  // Create a typed array view of the Uint32Array as Uint8Array
-  const uint8View = new Uint8Array(passcodes.buffer);
+    const passcodes = new Uint32Array(list_id);
+    // Create a typed array view of the Uint32Array as Uint8Array
+    const uint8View = new Uint8Array(passcodes.buffer);
 
-  // Create a data string using a temporary array and join
-  const dataString = Array.from(uint8View).map(byte => String.fromCharCode(byte)).join("");
+    // Create a data string using a temporary array and join
+    const dataString = Array.from(uint8View).map(byte => String.fromCharCode(byte)).join("");
 
-  // Encode the data string to Base64 using btoa
-  return btoa(dataString);
+    // Encode the data string to Base64 using btoa
+    return btoa(dataString);
 }
 
 function toYdkeURL() {
     let compresedDeck = {}
-    
-        compresedDeck.main = deck.main ? deck.main.map(el => el.id) : [];
-    
-        compresedDeck.extra = deck.extra ? deck.extra.map(el => el.id) : [];
-        compresedDeck.side = deck.side ? deck.side.map(el => el.id) : [];
+
+    compresedDeck.main = deck.main ? deck.main.map(el => el.id) : [];
+
+    compresedDeck.extra = deck.extra ? deck.extra.map(el => el.id) : [];
+    compresedDeck.side = deck.side ? deck.side.map(el => el.id) : [];
     return "ydke://" +
         passcodesToBase64(compresedDeck.main) + "!" +
         passcodesToBase64(compresedDeck.extra) + "!" +
@@ -142,25 +143,31 @@ async function getListCards(set_name) {
 }
 
 
+
 setsSelect.onchange = async function () {
 
     let selectedSets = Array.from(setsSelect.selectedOptions).map(x => x.value);
     console.log("Sets selected: ", selectedSets)
-    avalableCards = {};
-    for (const id in allCards) {    
+    let availableCards = []
+
+    for (const id in allCards) {
         let in_set = false;
         card = allCards[id];
         if (!card.card_sets || !Array.isArray(card.card_sets)) {
             continue;
         }
         for (const card_set of card.card_sets) {
-            if (selectedSets.includes(card_set.set_name)) {  
+            if (selectedSets.includes(card_set.set_name)) {
                 in_set = true;
             }
         }
         // console.log("Finding ", card_set.set_name , " in ", selectedSets)
-        if (in_set) avalableCards[id] = allCards[id];
+        if (in_set) availableCards.push(allCards[id]);
     }
+    availableCards.sort(function (x, y) {
+        if (x.name < y.name) return -1;
+        return 1;
+    });
     const availableContainer = document.getElementById("availableContainer");
     availableContainer.innerHTML = ""
     const headerRow = document.createElement("tr");
@@ -170,33 +177,49 @@ setsSelect.onchange = async function () {
     const elementHeader = document.createElement("th");
     elementHeader.textContent = "Card name";
     headerRow.appendChild(elementHeader);
+    const priceHeader = document.createElement("th");
+    priceHeader.textContent = "Price"
+    headerRow.appendChild(priceHeader)
+    const typeHeader = document.createElement("th");
+    typeHeader.textContent = "Type"
+    headerRow.appendChild(typeHeader);
     availableContainer.appendChild(headerRow);
 
-    for (const cardId in avalableCards)  {
-        const card = avalableCards[cardId];
+
+    for (const card of availableCards) {
         const row = document.createElement("tr");
 
         // Delete button cell
         const addCell = document.createElement("td");
         const addButton = document.createElement("button");
-        addButton.textContent = "Add";
+        addButton.textContent = "A";
         addButton.addEventListener("click", () => {
             // Update remaining elements to reflect updated array
-            deck.main.push(card);
-            populateDeckTable(deck.main);
+            addCard(card, false);
+            populateDeckTable();
         });
         addCell.appendChild(addButton);
         row.appendChild(addCell);
 
         // Element cell
         const elementCell = document.createElement("td");
-        elementCell.textContent = card.name;
+        elementCell.innerHTML = "<a href=\"" + card.ygoprodeck_url + "\"    target=\"_blank\">" + card.name + "</a>";
         row.appendChild(elementCell);
 
+        const priceCell = document.createElement("td");
+        const matched_set = findSet(card);
+        priceCell.textContent = matched_set.set_rarity_code + " - " + prices[matched_set.set_rarity_code];
+        row.appendChild(priceCell)
+
+        const typeCell = document.createElement("td");
+        typeCell.textContent = card.type;
+        row.appendChild(typeCell);
         availableContainer.appendChild(row);
     }
 
 }
+
+
 
 function findSet(card) {
     let selectedSets = Array.from(setsSelect.selectedOptions).map(x => x.value);
@@ -216,9 +239,28 @@ function findSet(card) {
 }
 async function validateDeck(deck) {
     let deck_price = 0;
-    for (const cardInfo of deck) {
+    if (!deck.side) deck.side = [];
+    if (!deck.main) deck.main = [];
+    if (!deck.extra) deck.extra = [];
+    for (const cardInfo of deck.main) {
         //console.log("Getting info of card: ", cardId)
-        if (deck.filter(el => el.id === cardInfo.id).length > 3) throw "INVALID: More than 3 cards of " + cardInfo.name;
+        if (deck.main.filter(el => el.id === cardInfo.id).length + deck.extra.filter(el => el.id === cardInfo.id).length + deck.side.filter(el => el.id === cardInfo.id).length > 3) throw "INVALID: More than 3 cards of " + cardInfo.name;
+        console.log(JSON.stringify(cardInfo));
+        console.log(cardInfo.name, cardInfo.id);
+        let matched_set = findSet(cardInfo);
+        deck_price += prices[matched_set.set_rarity_code];
+    }
+    for (const cardInfo of deck.extra) {
+        //console.log("Getting info of card: ", cardId)
+        if (deck.main.filter(el => el.id === cardInfo.id).length + deck.extra.filter(el => el.id === cardInfo.id).length + deck.side.filter(el => el.id === cardInfo.id).length > 3) throw "INVALID: More than 3 cards of " + cardInfo.name;
+        console.log(JSON.stringify(cardInfo));
+        console.log(cardInfo.name, cardInfo.id);
+        let matched_set = findSet(cardInfo);
+        deck_price += prices[matched_set.set_rarity_code];
+    }
+    for (const cardInfo of deck.side) {
+        //console.log("Getting info of card: ", cardId)
+        if (deck.main.filter(el => el.id === cardInfo.id).length + deck.extra.filter(el => el.id === cardInfo.id).length + deck.side.filter(el => el.id === cardInfo.id).length > 3) throw "INVALID: More than 3 cards of " + cardInfo.name;
         console.log(JSON.stringify(cardInfo));
         console.log(cardInfo.name, cardInfo.id);
         let matched_set = findSet(cardInfo);
@@ -227,23 +269,30 @@ async function validateDeck(deck) {
     if (deck_price > budget) throw "INVALID: Deck price $" + deck_price + " exceding budget of $" + budget;
     textContainer.textContent = "VALID: Deck price $" + deck_price;
 };
-
-
-function populateDeckTable(list_cards) {
-    list_cards = list_cards.sort(function(x, y) {
-        if (x.name < y.name) return -1;
-        return 1;
-    });
-    const listContainer = document.getElementById("listContainer");
-    listContainer.innerHTML = "";
-    let price = 0;
+function addCard(card, is_side) {
+    if (is_side) {
+        deck.side.push(card);
+    } else {
+        if (extraTypes.includes(card.type)) deck.extra.push(card)
+        else deck.main.push(card);
+    }
+}
+function BuildTable(subdeck, title, is_side) {
+    console.log(deck ,title)
+    let price = 0; 
+    const divTable = document.createElement("div");
     const info = document.createElement("div");
-    info.innerHTML = "<p>Number of cards: " + list_cards.length + " </p><p>price: " + price + "</p>";
-    listContainer.appendChild(info) 
+   // info.innerHTML = "<p>Number of cards: " + subdeck.length + " </p><p>price: " + price + "</p>";
+    info.innerHTML = title;
+
+    divTable.appendChild(info)
     const headerRow = document.createElement("tr");
     const deleteHeader = document.createElement("th");
     deleteHeader.textContent = "Delete";
     headerRow.appendChild(deleteHeader);
+    const moveHeader = document.createElement("th");
+    moveHeader.textContent = "Move";
+    headerRow.appendChild(moveHeader);
     const elementHeader = document.createElement("th");
     elementHeader.textContent = "Card name";
     headerRow.appendChild(elementHeader);
@@ -253,23 +302,36 @@ function populateDeckTable(list_cards) {
     const priceHeader = document.createElement("th");
     priceHeader.textContent = "Price";
     headerRow.appendChild(priceHeader);
-    listContainer.appendChild(headerRow);
+    divTable.appendChild(headerRow);
 
-    for (const card of list_cards) {
+    for (const card of subdeck) {
         const row = document.createElement("tr");
 
         // Delete button cell
         const deleteCell = document.createElement("td");
         const deleteButton = document.createElement("button");
-        deleteButton.textContent = "Remove";
+        deleteButton.textContent = "Delete";
+        deleteButton.style = "background-color: #ae2626;"
         deleteButton.addEventListener("click", () => {
             // Update remaining elements to reflect updated array
-            list_cards.splice(list_cards.indexOf(card), 1);
-            populateDeckTable(list_cards);
+            subdeck.splice(subdeck.indexOf(card), 1);
+            populateDeckTable();
         });
         deleteCell.appendChild(deleteButton);
         row.appendChild(deleteCell);
 
+        const moveCell = document.createElement("td");
+        const moveButton = document.createElement("button");  
+        moveButton.textContent = is_side ? "To main" : "To side";
+        moveButton.style = "background-color: #00cc00;"
+        moveButton.addEventListener("click", () => {
+            // Update remaining elements to reflect updated array
+            subdeck.splice(subdeck.indexOf(card), 1);
+            addCard(card, !is_side);
+            populateDeckTable();
+        });
+        moveCell.appendChild(moveButton);
+        row.appendChild(moveCell);        
         // Element cell
         const elementCell = document.createElement("td");
         elementCell.textContent = card.name;
@@ -286,10 +348,36 @@ function populateDeckTable(list_cards) {
         priceCell.textContent = prices[matched_set.set_rarity_code];
         row.appendChild(priceCell);
         price += prices[matched_set.set_rarity_code];
-        info.innerHTML = "<p>Number of cards: " + list_cards.length + "</p><p>price: " + price + "</p>";
-        
-        listContainer.appendChild(row);
+        divTable.appendChild(row);
     }
+    return {price, divTable};
+}
+
+function populateDeckTable() {
+    if (!deck.side) deck.side = [];
+    if (!deck.main) deck.main = [];
+    if (!deck.extra) deck.extra = [];
+    deck.main.sort(function (x, y) {
+        if (x.name < y.name) return -1;
+        return 1;
+    });
+    deck.extra.sort(function (x, y) {
+        if (x.name < y.name) return -1;
+        return 1;
+    });
+    deck.side.sort(function (x, y) {
+        if (x.name < y.name) return -1;
+        return 1;
+    });
+    const listContainer = document.getElementById("listContainer");
+    listContainer.innerHTML = "";
+    const main_table =  BuildTable(deck.main, "Main Deck", false)
+    const extra_table = BuildTable(deck.extra, "Extra Deck", false)
+    const side_table = BuildTable(deck.side, "Side Deck", true)
+    listContainer.appendChild(main_table.divTable);
+    listContainer.appendChild(extra_table.divTable);
+    listContainer.appendChild(side_table.divTable);
+ 
 };
 
 
@@ -302,12 +390,22 @@ importButton.addEventListener('click', async function () {
     }
     try {
         parsed_deck = parseYdkeUrl(text);
-        deck.main = []
-        for (let id of parsed_deck.main) {
+        deck.main = []  
+        deck.extra = []
+        deck.side = []
+       for (let id of parsed_deck.main) {
             if (allCards[id]) deck.main.push(allCards[id])
             else throw "INVALID: Card " + id + " not found in ygopro database";
-        }
-        populateDeckTable(deck.main);
+        } 
+        for (let id of parsed_deck.extra) {
+            if (allCards[id]) deck.extra.push(allCards[id])
+            else throw "INVALID: Card " + id + " not found in ygopro database";
+        } 
+        for (let id of parsed_deck.side) {
+            if (allCards[id]) deck.side.push(allCards[id])
+            else throw "INVALID: Card " + id + " not found in ygopro database";
+        } 
+        populateDeckTable();
 
     } catch (err) {
         console.log(err);
@@ -318,16 +416,16 @@ importButton.addEventListener('click', async function () {
 validateButton.addEventListener('click', async function () {
 
     try {
-        await validateDeck(deck.main);
+        await validateDeck(deck);
     } catch (err) {
         textContainer.textContent = err;
     }
 });
-exportButton.addEventListener('click', async function() {
+exportButton.addEventListener('click', async function () {
     try {
         navigator.clipboard.writeText(toYdkeURL());
         alert("Ydke copied to clipboard");
-    } catch(err) {
+    } catch (err) {
         alert(err);
     }
 })
